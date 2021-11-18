2d8d1a884cdf60fdc74a14eb983f00199ecb7583
Ahmet Talha TURKMEN <75725469+AhmetTalhaTURKMEN@users.noreply.github.com>
2021-11-18 15:04:47 +0300

    Update 1-)Linked list Length.c

--- 1-)Linked list Length.c
+++ 1-)Linked list Length.c
@@ -8,7 +8,7 @@ struct node {
 } *head;
 
 
-void insert(int num) {//asil odev linked list uzunlugu bulmak oldugu icin linked liste eleman eklemek icin internetten biraz yardim aldim
+void insert(int num) {
     struct node* newNode = (struct node*) malloc(sizeof(struct node));
     newNode->data  = num;
     newNode->next = head;
